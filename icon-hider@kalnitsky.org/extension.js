fb2bc085d1ac1c57ca99076de76f4989f24690cb
Evgeni (Rush) Nepomniashchi <rush.zlo@gmail.com>
2012-10-04 16:48:10 +0400

    add support for preferences dialog

--- icon-hider@kalnitsky.org/extension.js
+++ icon-hider@kalnitsky.org/extension.js
@@ -23,12 +23,22 @@ const Main = imports.ui.main
 const Panel = imports.ui.panel
 const PopupMenu = imports.ui.popupMenu
 const PanelMenu = imports.ui.panelMenu
+const Mainloop = imports.mainloop;
+const Lang = imports.lang;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Params = imports.misc.params;
+
+const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
 
 // global consts
 const EXTENSION_NAME = 'Icon Hider'
 const GSETTINGS_SCHEMA = 'org.gnome.shell.extensions.icon-hider'
-const GSETTINGS_HIDE_KEY = 'hided-icons'
+const GSETTINGS_HIDDEN_ICONS = 'hided-icons'
+const GSETTINGS_SEEN_ICONS = 'seen-icons'
 
+// interval to force hide unwanted actors
+const INDICATOR_UPDATE_INTERVAL = 5000;
 
 /*
  * Indicator definition.
@@ -49,42 +59,49 @@ Indicator.prototype = {
      * Constructor
      */
     _init: function() {
-        PanelMenu.SystemStatusButton.prototype._init.call(this, 'view-grid')
+        PanelMenu.SystemStatusButton.prototype._init.call(this, 'view-grid');
 
         // exceptions are not show in menu
-        this._exceptions = ['battery', 'message-notifier']
+        this._exceptions = ['battery', 'message-notifier'];
 
-        this._initSettings()
-        this._createMenu()
+        this._settings = Convenience.getSettings();
+
+        this.knownIcons = [];
+
+        // hook addToStatusArea
+        this._MainPanelAddToStatusArea = Main.panel.addToStatusArea;
+        Main.panel.addToStatusArea = Lang.bind(this, this._addToStatusArea);
+
+        this._refreshIcons();
+        this._createMenu();
+
+        this._settings.connect("changed::" + GSETTINGS_HIDDEN_ICONS, Lang.bind(this, function() {
+            global.log("*** settings changed");
+            this._refreshIcons();
+        }));
+
+        // SHITFIX: network icon arrive after some time
+        // TODO: put this to prefs
+        this._timeout = Mainloop.timeout_add(INDICATOR_UPDATE_INTERVAL, Lang.bind(this, function () {
+            global.log("*** TIMEOUT REACHED");
+            this._refreshIcons();
+            return false; // TODO: put this to prefs too
+        }))
     },
 
-    /**
-     * Get settings instance and save it as `this._settings`
-     */
-    _initSettings: function() {
-        let extension = imports.misc.extensionUtils.getCurrentExtension()
-
-        let src = Gio.SettingsSchemaSource.new_from_directory(
-            extension.dir.get_child('schemas').get_path(),
-            Gio.SettingsSchemaSource.get_default(),
-            false
-        )
-
-        this._settings = new Gio.Settings({
-            settings_schema: src.lookup(GSETTINGS_SCHEMA, false)
-        })
+    destroy: function() {
+        Mainloop.source_remove(this._timeout);
+        Main.panel.addToStatusArea = this._MainPanelAddToStatusArea;
+        this._MainPanelAddToStatusArea = null;
     },
 
     /**
      * Create menu in top bar.
      */
     _createMenu: function() {
-        let hiddenItems = this._settings.get_strv(GSETTINGS_HIDE_KEY)
+        let hiddenItems = this._settings.get_strv(GSETTINGS_HIDDEN_ICONS)
 
         for (let item in Main.panel._statusArea) {
-            // don't add myself
-            if (item == EXTENSION_NAME)
-                continue
             // don't add exceptions (this is items not works correctly)
             if (this._exceptions.indexOf(item) != -1)
                 continue
@@ -117,11 +134,11 @@ Indicator.prototype = {
      * Hide element and mark it as hidden in settings.
      */
     _hideItem: function (item) {
-        let hiddenItems = this._settings.get_strv(GSETTINGS_HIDE_KEY)
+        let hiddenItems = this._settings.get_strv(GSETTINGS_HIDDEN_ICONS)
 
         if (hiddenItems.indexOf(item.statusAreaKey) == -1) {
             hiddenItems.push(item.statusAreaKey)
-            this._settings.set_strv(GSETTINGS_HIDE_KEY, hiddenItems)
+            this._settings.set_strv(GSETTINGS_HIDDEN_ICONS, hiddenItems)
         }
 
         item.statusAreaItem.actor.hide()
@@ -131,12 +148,12 @@ Indicator.prototype = {
      * Show element and mark it as visible in settings.
      */
     _showItem: function (item) {
-        let hiddenItems = this._settings.get_strv(GSETTINGS_HIDE_KEY)
+        let hiddenItems = this._settings.get_strv(GSETTINGS_HIDDEN_ICONS)
         let index = hiddenItems.indexOf(item.statusAreaKey)
 
         while (index != -1) {
             hiddenItems.splice(index, 1)
-            this._settings.set_strv(GSETTINGS_HIDE_KEY, hiddenItems)
+            this._settings.set_strv(GSETTINGS_HIDDEN_ICONS, hiddenItems)
             index = hiddenItems.indexOf(item.statusAreaKey)
         }
 
@@ -152,7 +169,44 @@ Indicator.prototype = {
 
         for (let index in menuItems)
             menuItems[index].statusAreaItem.actor.show()
-    }
+    },
+
+    _refreshIcons: function() {
+        let hiddenIcons = this._settings.get_strv(GSETTINGS_HIDDEN_ICONS)
+        let updatePrefs = false;
+
+        for (let role in Main.panel._statusArea) {
+
+
+            if (this._exceptions.indexOf(role) != -1) continue;
+
+            if (this.knownIcons.indexOf(role) == -1) {
+                this.knownIcons.push(role);
+                updatePrefs = true;
+                global.log("*** detected new icon: " + role);
+            }
+
+            if (hiddenIcons.indexOf(role) != -1) {
+                Main.panel._statusArea[role].actor.hide();
+            } else {
+                Main.panel._statusArea[role].actor.show();
+            }
+
+        }
+
+        // update settings to make icon list visible to prefs dialog
+        if (updatePrefs) {
+            global.log("*** update known icon list");
+            this._settings.set_strv(GSETTINGS_SEEN_ICONS, this.knownIcons);
+        }
+    },
+
+    _addToStatusArea: function(role, indicator, position) {
+        let result = this._MainPanelAddToStatusArea.call(Main.panel, role, indicator, position);
+        this._refreshIcons();
+        return result;
+    },
+
 }
 
 
